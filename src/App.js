23667dd8157f5928c7ed5942017cccb8ff9ac275
santana-devin <fabriciosantana.devin@gmail.com>
2022-06-01 16:22:16 -0300

    Aula 1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello Word</Text>
+    </View>
+  );
+};
+
+export default App;
