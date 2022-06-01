185e9c9a8c984426dd56abaf14eaaaa0cbe0e44e
santana-devin <fabriciosantana.devin@gmail.com>
2022-06-01 16:30:28 -0300

    Aula 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import {View,Text} from 'react-native';
+import {View,Text, StyleSheet} from 'react-native';
 
 
 const App = () => {
   return (
-    <View>
-      <Text>Hello Word</Text>
+    <View style={style.container}>
+      <Text style={style.text}>Hello Word</Text>
     </View>
   );
 };
+
+const style = StyleSheet.create({
+  container:{
+    backgroundColor: 'blue',
+    flex:1, //Expande o container para a tale inteira
+  },
+  text:{
+    fontSize:50,
+    fontWeight: '900',
+    color:'black'
+
+  }
+});
 
 export default App;
